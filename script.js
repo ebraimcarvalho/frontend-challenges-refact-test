237b855ef864adf44615bbc1757e37cb6aa5ab65
ebraimcarvalho <ebraimcarvalho@gmail.com>
2020-04-10 17:05:14 -0300

    main index.html refactoring html and css for BEM

--- script.js
+++ script.js
@@ -42,7 +42,7 @@
       <a href="./${name}/index.html" target="_blank">
         <h4>${i + 2}. ${formatName(name)}</h4>
       </a>
-      <p>This is a challenge to improve my frontend skills! Technical details and more informations, you can access on page: 
+      <p class="card-project__description">This is a challenge to improve my frontend skills! Technical details and more informations, you can access on page: 
         <a href="${linkChallenge}" target="_blank">Page of Challenge;</a> 
         Or access the 
         <a href="https://github.com/ebraimcarvalho/frontend-challenges/tree/master/${name}" target="_blank">Code page - Github</a></p>
